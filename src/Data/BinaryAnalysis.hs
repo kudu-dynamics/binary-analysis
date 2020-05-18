d4f5630de087cf4ac3d2ee54a6ee0aeb04f664da
drone <drone@kududyn.com>
2020-05-18 07:58:55 -0600

    Added Symbol type. Addresses now have a Hashable instance.

--- src/Data/BinaryAnalysis.hs
+++ src/Data/BinaryAnalysis.hs
@@ -1,14 +1,24 @@
 module Data.BinaryAnalysis where
 
+import Data.Hashable
+import Data.Text
+import Data.Word
 import GHC.Generics
 import qualified Numeric
-import Data.Hashable
-import Data.Word
 
 newtype Address = Address Word64
   deriving (Eq, Ord, Num, Real, Enum, Integral, Generic)
 
 instance Show Address where
-  show (Address x) =  showString "Address 0x" . Numeric.showHex x $ ""
+  show (Address x) = showString "Address 0x" . Numeric.showHex x $ ""
 
 instance Hashable Address
+
+data Symbol
+  = Symbol
+      { _symbolName :: Text,
+        _symbolRawName :: Text
+      }
+  deriving (Eq, Ord, Show, Generic)
+
+instance Hashable Symbol
