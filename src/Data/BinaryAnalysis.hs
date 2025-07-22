18a7534585ff9fe8f7156c2f1023bb9cd7d88d03
rudy <rsorensen@kududyn.com>
2025-07-22 12:24:29 -0400

    remove automatically derived Eq

--- src/Data/BinaryAnalysis.hs
+++ src/Data/BinaryAnalysis.hs
@@ -85,7 +85,7 @@ data Address = Address
   { space :: AddressSpace
   , offset :: Int64 -- ^ multiply by addressableUnitSize to get byte offset
   }
-  deriving (Eq, Ord, Read, Generic, Hashable)
+  deriving (Ord, Read, Generic, Hashable)
   deriving anyclass (FromJSON, ToJSON)
 
 addrToInt :: Address -> Int64
